84fe302b8c76b34dc3c575b5629687c35a01d883
dzmirtyklimenak <dzmitry.klimenak@capital.com>
2024-02-25 10:11:44 +0200

    feat: add todo

--- src/app/todo-manager/todo-manager-layout/todo-manager-layout.component.ts
+++ src/app/todo-manager/todo-manager-layout/todo-manager-layout.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-todo-manager-layout',
   templateUrl: './todo-manager-layout.component.html',
-  styleUrls: ['./todo-manager-layout.component.scss']
+  styleUrls: ['./todo-manager-layout.component.scss'],
 })
-export class TodoManagerLayoutComponent {
-
-}
+export class TodoManagerLayoutComponent {}
